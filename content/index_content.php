f67ccf39b2c5d342cb343ea2c83539c288c2ad71
Jean-Bernard <label5@hotmail.com>
2022-01-31 22:18:29 +0100

    mofi box size cta inner

--- content/index_content.php
+++ content/index_content.php
@@ -16,7 +16,7 @@
         <section class="page-section cta">
             <div class="container">
                 <div class="row">
-                    <div class="col-xl-8 mx-auto">
+                    <div class="col-xl-9 mx-auto">
                         <div class="cta-inner bg-faded text-center rounded">
                             <h2 class="section-heading mb-4">
                                 <span class="section-heading-upper">Chocolatier depuis</span>
